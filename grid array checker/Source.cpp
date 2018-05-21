201e5ca5e61b726207ebf7e73731d1e5c5cdcde5
Mondotrasho <oscar@peoplepictures.com.au>
2018-05-21 14:00:41 +1000

    renamed variables for clarity and added comments for transparency

--- grid array checker/Source.cpp
+++ grid array checker/Source.cpp
@@ -3,19 +3,24 @@
 #include <windows.h>
 
 using namespace std;
-#define player p;
+
 class Player
 {
 public:
 	int x, y;
-	void move_m(int newx, int newy)
+	void m_Player(int newx, int newy) //Moves player Takes new X and Y and Clamps within range
 	{
+		int Xmax = 41;
+		int Xmin = -1;
+		int Ymax = 3;
+		int Ymin = -1;
+
 		x = newx;
-		if (x >= 41) { x = 40; }
-		if (x <= -1) { x = 0; }
+		if (x >= Xmax) { x = Xmax-1; }
+		if (x <= Xmin) { x = Xmin +1; }
 		y = newy;
-		if (y >= 3) { y = 2; }
-		if (y <= -1) { y = 0; }
+		if (y >= Ymax) { y = Ymax - 1; }
+		if (y <= Ymin) { y = Ymin - 1; }
 	};
 };
 class Map
@@ -25,65 +30,64 @@ public:
 		{ 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 },//sky
 		{ 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1 },//middle
 		{ 1,0,1,1,1,1,1,0,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,0,0,1,1,1,1,1,1,1,1,1,1 } };//ground
-	bool movecheck(int x, int y)
+	bool location_check(int Potential_x, int Potential_y) //checks if a move is valid by taking in a potential new X and Y Coordinate and checking the Map array
 	{
-		if (collision[y][x] == true)
-		{
-			return true;
-		}
-		else {
-			return false;
-		}
+		if (collision[Potential_y][Potential_x] == true) //WATCH ORDER OF XY
+		{return true;}
+		else {return false;}
 	};
 };
 
 int main()
 {
 	Map world;
-	Player p;
-	p.x = 4;
-	p.y = 2;
+	Player player;
+	player.x = 4; //starting x
+	player.y = 2; //starting y		Add default constructor in final
 
 	while(true)
 	{
+		//sets the int to GetAsyncKeyState for each key ##NOTE## Aparently what it outputs is a short where 
+		//the binary sets the first bit to 1 so in binary it is 1000 0000 0000 0000 but when I convert it to in its -3k or so
 		int left = GetAsyncKeyState(VK_LEFT);
 		int right = GetAsyncKeyState(VK_RIGHT);
 		int up = GetAsyncKeyState(VK_UP);
 		int down = GetAsyncKeyState(VK_DOWN);
 
+		//use if statements to filter for potential key presses
 		if(left != 0)
 		{
-			bool maymove = world.movecheck((p.x - 1), p.y);
-			if(maymove == true)
+			bool maymove = world.location_check((player.x - 1), player.y); //calls may move function
+			if(maymove == true)											   //if you can move
 			{
-				p.move_m((p.x - 1), p.y);
-			}
+				player.m_Player((player.x - 1), player.y);				   //send new x and y cords to plays Move player function 
+			}															   //##NOT CHANGING VALUES DIRECTLY AS THAT MIGHT CAUSE FUCK UPS DOWN THE LINE##
 		}
 		else if (right != 0)
 		{
-			bool maymove = world.movecheck((p.x + 1), p.y);
+			bool maymove = world.location_check((player.x + 1), player.y);
 			if (maymove == true)
 			{
-				p.move_m((p.x + 1), p.y);
+				player.m_Player((player.x + 1), player.y);
 			}
 		}
 		else if (up != 0)
 		{
-			bool maymove = world.movecheck(p.x, (p.y - 1));
+			bool maymove = world.location_check(player.x, (player.y - 1));
 			if (maymove == true)
 			{
-				p.move_m(p.x, (p.y - 1));
+				player.m_Player(player.x, (player.y - 1));
 			}
 		}
 		else if (down != 0)
 		{
-			bool maymove = world.movecheck(p.x, (p.y + 1));
+			bool maymove = world.location_check(player.x, (player.y + 1));
 			if (maymove == true)
 			{
-				p.move_m(p.x, (p.y + 1));
+				player.m_Player(player.x, (player.y + 1));
 			}
 			
 		}
-		cout << p.x << "-" << p.y << endl;
+		cout << player.x << "-" << player.y << endl; //to debug key presses
 	}
 }
